e1fbc85ea4a31c45a3858facba0e33f157527379
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-10 10:08:02 +0200

    Ajout de la classe Commentaire

---
+++ app/Models/Commentaire.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use PDO;
+
+class Commentaire extends Model {
+    public function getByJpo($jpo_id) {
+        $stmt = $this->pdo->prepare("SELECT c.*, u.nom, u.prenom FROM commentaire c JOIN utilisateur u ON c.utilisateur_id = u.id WHERE jpo_id = ? ORDER BY date_commentaire DESC");
+        $stmt->execute([$jpo_id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function add($utilisateur_id, $jpo_id, $contenu, $parent_id = null) {
+        $stmt = $this->pdo->prepare("INSERT INTO commentaire (utilisateur_id, jpo_id, contenu, parent_id) VALUES (?, ?, ?, ?)");
+        return $stmt->execute([$utilisateur_id, $jpo_id, $contenu, $parent_id]);
+    }
+}
+?>
