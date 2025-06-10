39554cada41a83c093ff02ddf79fd468d744a4ba
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-10 10:34:44 +0200

    Ajout du fichier database qui va peremettre la connexion à la base de donnée

---
+++ app/core/Database.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $config = require dirname(__DIR__, 2) . '/config/config.php';
+
+        try {
+            $this->pdo = new PDO(
+                "mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8",
+                $config['db_user'],
+                $config['db_pass'],
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(): PDO {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance->pdo;
+    }
+}
+?>
