c0bba62266db51118d58b70ede64e671bc881f6f
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-16 11:20:52 +0200

    Ajout de la fonction pour obtenir l'établissement avec l'id

--- app/Models/Etablissement.php
+++ app/Models/Etablissement.php
@@ -8,5 +8,12 @@ class Etablissement extends Model {
         $stmt = $this->pdo->query("SELECT * FROM etablissement");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM etablissement WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
 ?>
