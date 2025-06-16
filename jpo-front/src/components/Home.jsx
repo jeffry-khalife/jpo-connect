0d2c9590cdf3157cbd4af850b6552cee4c35b902
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-16 11:27:24 +0200

    Ajout de la page d'acceuil

---
+++ jpo-front/src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Journées Portes Ouvertes</h1>
+      <p>Nos écoles vous ouvrent leurs portes, rencontrez nous et discutons de votre avenir</p>
+      <a href="/jpos"><img src="https://laplateforme.io/wp-content/uploads/2024/01/JPO-Image-mise-en-avant-wordpress.png" alt="" /></a>
+    </div>
+  );
+}
