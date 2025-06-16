a249cfc8bfb2939a39c75170285be289961ecf9e
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-16 11:16:50 +0200

    Ajout de la fonction pour afficher les villes

--- app/Controllers/EtablissementController.php
+++ app/Controllers/EtablissementController.php
@@ -7,18 +7,20 @@ class EtablissementController {
     public function index() {
         $model = new Etablissement();
         $list = $model->getAll();
+        header('Content-Type: application/json');
         echo json_encode($list);
     }
 
     public function show($id) {
-        $model = new Etablissement();
-        $item = $model->getById($id);
-        if ($item) {
-            echo json_encode($item);
-        } else {
-            http_response_code(404);
-            echo json_encode(['error' => 'Etablissement non trouvé']);
-        }
+    $model = new Etablissement();
+    $item = $model->getById($id);
+    header('Content-Type: application/json');
+    if ($item) {
+        echo json_encode($item);
+    } else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Etablissement non trouvé']);
     }
+}
 }
 ?>
