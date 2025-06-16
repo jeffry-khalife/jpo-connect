3d13546952c89c435c8d8cf6c798bb5f1b2eb7a9
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-16 11:18:24 +0200

    Ajout de la fonction pour supprimer une jpo

--- app/Controllers/JpoController.php
+++ app/Controllers/JpoController.php
@@ -23,9 +23,23 @@ class JpoController {
 
     public function create() {
         $data = json_decode(file_get_contents('php://input'), true);
+        $data['role_id'] = 3;
         $jpo = new Jpo();
         $success = $jpo->create($data['titre'], $data['description'], $data['date_jpo'], $data['capacite'], $data['etablissement_id']);
         echo json_encode(['success' => $success]);
     }
+
+    public function delete($id) {
+    $model = new \App\Models\Jpo();
+    $success = $model->delete($id);
+    if ($success) {
+        http_response_code(200);
+        echo json_encode(['success' => true]);
+    } else {
+        http_response_code(400);
+        echo json_encode(['success' => false, 'message' => "Suppression impossible"]);
+    }
+    }
+
 }
 ?>
